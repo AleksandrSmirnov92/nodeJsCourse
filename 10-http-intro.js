81bd3884e2b30001241410e06f2f95fd285b235a
AleksandrSmirnov92 <ryan00@mail.ru>
2022-07-20 17:42:59 +0300

    node.js

--- 10-http-intro.js
+++ 10-http-intro.js
@@ -1,4 +1,18 @@
 const http = require("http"); /**подключаем */
 const server = http.createServer((req, res) => {
-  /**оба аргумента обьекты  req - входящий запрос ,responce - ответ от сервера*/
+  /**оба аргумента обьекты  req - входящий запрос это гиганский обьект ,responce - ответ от сервера*/
+  console.log(req);
+  if (req.url === "/") {
+    res.end("hello this my first server");
+  }
+  if (req.url === "/about") {
+    res.end("this page about me");
+  }
+  req.end(`
+     <h1>OOps! mybe try again write current page</h1>
+     <a href="/"> back page</a>
+     `);
+  //   res.write("welcome");
+  //   res.end();
 }); /**создаем */
+server.listen(5000); /**название сервера */
