35287de3fde806e6ad2c144fe0ad2d85dca4a020
AleksandrSmirnov92 <ryan00@mail.ru>
2022-08-03 16:31:33 +0300

    учу express

--- express.ts
+++ express.ts
@@ -1,22 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
-  const a = 4;
-  if (a > 5) {
-    res.send("Hello World!");
-  } else {
-    res.send("lower 5");
-  }
+app.get('/', (req: any, res: { send: (arg0: { message: string }) => void }) => {
+  res.send({ message: 'hello world' });
 });
-app.get("/home", (req: any, res: { send: (arg0: string) => void }) => {
-  res.send("My home");
-});
-app.post("/sumuray", (req: any, res: { send: (arg0: string) => void }) => {
-  res.send("We created samurai!");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
